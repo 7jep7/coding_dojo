38f55cacf14da6636cb90dadfe36543135796de2
7jep7 <je77petersen@gmail.com>
2025-08-20 16:17:31 -0400

    Refactor: move and structure tests for count_fragment_combinations using unittest

--- basic_python.py
+++ basic_python.py
@@ -11,41 +11,7 @@ def count_fragment_combinations(fragments, message):
     """
     pass  # TODO: Implement the solution
 
-def test_count_fragment_combinations():
-    # Test 1: Example from docstring
-    fragments = [1, 23, 4]
-    message = 1234
-    assert count_fragment_combinations(fragments, message) == 1
-
-    # Test 2: Multiple ways
-    fragments = [12, 3, 4, 123, 34]
-    message = 1234
-    # Possible: [12, 34], [123, 4], [1, 23, 4]
-    # But [1, 23, 4] not possible since 1 not in fragments
-    assert count_fragment_combinations(fragments, message) == 2
-
-    # Test 3: No possible combinations
-    fragments = [5, 6, 7]
-    message = 123
-    assert count_fragment_combinations(fragments, message) == 0
-
-    # Test 4: Fragments can only be used once
-    fragments = [1, 1, 2]
-    message = 112
-    assert count_fragment_combinations(fragments, message) == 2  # [1,1,2] (two ways to pick the 1s)
-
-    # Test 5: Empty fragments
-    fragments = []
-    message = 1
-    assert count_fragment_combinations(fragments, message) == 0
-
-if __name__ == "__main__":
-    test_count_fragment_combinations()
-    print("All tests passed.")
-
-print("Hello, Coding Dojo!")
-for i in range(5):
-    print(f"Number {i}")
+# The tests have been moved to test_basic_python.py for better structure and maintainability.
 
 
 
---
+++ test_basic_python.py
@@ -0,0 +1,31 @@
+import unittest
+from basic_python import count_fragment_combinations
+
+class TestCountFragmentCombinations(unittest.TestCase):
+    def test_example_from_docstring(self):
+        fragments = [1, 23, 4]
+        message = 1234
+        self.assertEqual(count_fragment_combinations(fragments, message), 1)
+
+    def test_multiple_ways(self):
+        fragments = [12, 3, 4, 123, 34]
+        message = 1234
+        self.assertEqual(count_fragment_combinations(fragments, message), 2)
+
+    def test_no_possible_combinations(self):
+        fragments = [5, 6, 7]
+        message = 123
+        self.assertEqual(count_fragment_combinations(fragments, message), 0)
+
+    def test_fragments_used_once(self):
+        fragments = [1, 1, 2]
+        message = 112
+        self.assertEqual(count_fragment_combinations(fragments, message), 2)
+
+    def test_empty_fragments(self):
+        fragments = []
+        message = 1
+        self.assertEqual(count_fragment_combinations(fragments, message), 0)
+
+if __name__ == "__main__":
+    unittest.main()
