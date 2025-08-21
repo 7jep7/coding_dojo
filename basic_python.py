c9dce861771a3edef4256e6ed3f1252b12b80b7e
7jep7 <je77petersen@gmail.com>
2025-08-21 11:41:03 -0400

    Implement and document count_fragment_combinations; tests are in separate file

--- basic_python.py
+++ basic_python.py
@@ -2,6 +2,12 @@
 
 # Problem: Given a list of integer fragments and a target integer message, count how many ways you can concatenate (not add) some of the fragments (in any order and using each at most once) to form the message as an integer. Do not implement the solution yet.
 
+def equal_start(fragment, message):
+    """
+    Check if the fragment starts with the same digits as the message.
+    """
+    return str(message).startswith(str(fragment))
+
 def count_fragment_combinations(fragments, message):
     """
     Given a list of integer fragments and a target integer message, count how many ways you can concatenate some of the fragments (in any order, using each at most once) to form the message.
@@ -9,9 +15,22 @@ def count_fragment_combinations(fragments, message):
     Possible combinations: [1, 23, 4] => 1|23|4 = 1234
     Return the count of such combinations.
     """
-    pass  # TODO: Implement the solution
+    occurences = 0
+
+    for f in fragments:
+        if equal_start(f, message):
+            if f == message:
+                occurences += 1
+            else:
+                new_fragments = fragments.copy()
+                new_fragments.remove(f)
+                occurences += count_fragment_combinations(new_fragments, int(str(message)[len(str(f)):]))
+
+    return occurences
 
 # The tests have been moved to test_basic_python.py for better structure and maintainability.
+
+
 
 
 
